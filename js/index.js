6f10317ff4813ae384a72d4ff488d496ddf967e9
morawiec004 <morawiec004@gmail.com>
2020-12-16 19:32:31 +0100

    Zajecia 16.12

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function changeInput() {
+function changeInput(event) {
     const todoInput = document.getElementById('todoInput');
     const todoButton = document.getElementById('inputButton');
 
@@ -6,5 +6,47 @@ function changeInput() {
         todoButton.style.display = 'none'; 
     } else {
         todoButton.style.display = 'inline';
+    }
+
+    if (event.keyCode == 13) {
+        addToDo();
+    }
+}
+
+function addToDo() {
+    const todoInput = document.getElementById('todoInput');
+    const todoListContainer = document.getElementById('todoListContainer');
+    const todoContainers = document.getElementsByClassName('todoContainer');
+    const inputButton = document.getElementById('inputButton');
+
+    todoListContainer.innerHTML += 
+        `<div class="todoContainer" id='todoContainer${todoContainers.length + 1}'>
+            <button class="todoButton" id='todoButton${todoContainers.length + 1}' onclick='deleteTodo(${todoContainers.length + 1})'>x</button>
+            <input type="checkbox" class="todoCheckbox" id='todoCheckbox${todoContainers.length + 1}' onchange='changeCheckbox(${todoContainers.length + 1})'>
+            <label class="todoLabel" id='todoLabel${todoContainers.length + 1}'>${todoInput.value}</label>
+        </div>`
+    todoInput.value = '';
+    inputButton.style.display = 'none';
+
+    const todos = JSON.parse(localStorage.getItem('todos') || '[]');
+
+    todos.push({id:1, todo: 'jakis tekst', done: false});
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function deleteTodo(id) {
+    const container = document.getElementById(`todoContainer${id}`);
+    container.remove();
+}
+
+function changeCheckbox(id) {
+    const checkbox = document.getElementById(`todoCheckbox${id}`);
+    const label = document.getElementById(`todoLabel${id}`);
+    
+    if (checkbox.checked) {
+        label.classList.add('todoLabelCrossed');
+    } else {
+        label.classList.remove('todoLabelCrossed');
     }
 }
